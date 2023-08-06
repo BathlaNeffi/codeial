c5eb604a1cf30e01ebf017dd9c702e71c654bd8b
invincible <neffybathla@gmail.com>
2023-08-06 21:59:08 +0530

    added the check of user must be sign in to add the post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 
 
 const postConroller=require('../controllers/posts_controller');
 
-router.post('/create',postConroller.create);
+router.post('/create',passport.checkAuthentication,postConroller.create);
 
 module.exports=router;
