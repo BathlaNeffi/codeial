e5f90f64240a41eb755d76c42c24b87fe66c7f9a
invincible <neffybathla@gmail.com>
2023-08-10 15:06:48 +0530

    added the delete option in comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -26,3 +26,35 @@ module.exports.create= async (req,res)=>{
 
     }
 }
+
+
+module.exports.destroy= async (req,res)=>{
+
+    try{
+         
+        const comment= await Comment.findById(req.params.id);
+       
+        let postId=comment.post;
+        const post= await Post.findById(postId);
+        
+        console.log('comment.user ' , comment.user ,'req.user.id',req.user.id);
+        console.log('post.user ' , post.user ,'req.user.id',req.user.id);
+        if( post.user == req.user.id   || comment.user == req.user.id  ){
+            
+
+
+            comment.deleteOne();
+            await Post.findByIdAndUpdate( postId , { $pull: {comments: req.params.id}})
+            return res.redirect('back');
+
+
+        }else{
+            console.log('err in delteing comment')
+            return res.redirect('back');
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+
+}
