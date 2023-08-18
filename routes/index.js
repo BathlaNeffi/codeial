653fdd9e8202389592e28ed6cf11bf1b90651381
invincible <neffybathla@gmail.com>
2023-08-18 22:55:32 +0530

    world of api's

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
 router.post('/save', homeController.saveProfile);
+router.use('/api',require('./api'));
+
 
 
 
